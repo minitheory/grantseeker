6f5aa897626b19a46b323db3949711507a2add6d
Sheldon Cheng <sheldonscyd@gmail.com>
2015-07-16 15:50:13 +0800

    Add selection function to buttons

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
   $('.ui.dropdown').dropdown();
 })
+
+$('.js-select a').click(function(e){
+  e.preventDefault();
+  $(this).addClass('active').siblings('.active').removeClass('active');
+});
